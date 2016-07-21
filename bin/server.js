e185ef824cd254718173d5571a254f8a0a0a08cc
Steve <steve5805@gmail.com>
2016-07-21 13:04:24 +0000

    cleanup server.js

--- bin/server.js
+++ bin/server.js
@@ -1,66 +1,37 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
-var app = require('../app');
-var debug = require('debug')('D20:server');
+// Load module dependencies.
 var http = require('http');
+var debug = require('debug')('D20:server');
+var app = require('../app');
 
-/**
- * Get port from environment and store in Express.
- */
-
+// Get port from environment and store in Express.
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
-
+// Create HTTP server and listen on provided port, on all network interfaces.
 var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
 
 /**
- * Normalize a port into a number, string, or false.
+ * Event listener for HTTP server "listening" event.
  */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
+server.on('listening', function() {
+  var addr = server.address();
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+  
+  debug('Listening on ' + bind);
+  var hostname = process.env.C9_HOSTNAME || require("os").hostname();
+  console.log(`Server is running at http://${hostname}:${addr.port}`);
+});
 
 /**
  * Event listener for HTTP server "error" event.
  */
+server.on('error', function(error) {
+  if (error.syscall !== 'listen') {throw error;}
 
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
@@ -75,17 +46,19 @@ function onError(error) {
     default:
       throw error;
   }
-}
+});
 
 /**
- * Event listener for HTTP server "listening" event.
+ * Normalizes a port into a number, string, or false.
  */
+function normalizePort(val) {
+  var port = parseInt(val, 10);
 
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-  var hostname = process.env.C9_HOSTNAME || require("os").hostname();
-  console.log(`Server is running at http://${hostname}:${addr.port}`);
-  console.log(server.address().address)
+  if (isNaN(port)) {  // named pipe
+    return val; 
+  } else if (port >= 0) { // port number
+    return port;  
+  } else { 
+    return false;
+  }
 }
